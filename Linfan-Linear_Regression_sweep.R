2a00bba5179d3172e05a0c45ef1fbe5fc38ccd64
linfanz <linfanz95@outlook.com>
2017-11-29 23:49:18 -0800

    add a test example

--- Linfan-Linear_Regression_sweep.R
+++ Linfan-Linear_Regression_sweep.R
@@ -112,34 +112,34 @@ myLinearRegression <- function(X, Y){
 ## Optional examples (comment out before submitting!) ##
 ########################################################
 
-# testing_Linear_Regression <- function(){
-#   
-#   ## This function is not graded; you can use it to 
-#   ## test out the 'myLinearRegression' function 
-# 
-#   ## Define parameters
-#   n    <- 100
-#   p    <- 3
-#   
-#   ## Simulate data from our assumed model.
-#   ## We can assume that the true intercept is 0
-#   X    <- matrix(rnorm(n * p), nrow = n)
-#   beta <- matrix(1:p, nrow = p)
-#   Y    <- X %*% beta + rnorm(n)
-#   
-#   ## Save R's linear regression coefficients
-#   R_coef  <- coef(lm(Y ~ X))
-#   
-#   ## Save our linear regression coefficients
-#   my_coef <- myLinearRegression(X, Y)
-#   
-#   ## Are these two vectors different?
-#   sum_square_diff <- sum((R_coef - my_coef)^2)
-#   if(sum_square_diff <= 0.001){
-#     return('Both results are identical')
-#   }else{
-#     return('There seems to be a problem...')
-#   }
-#   
-# }
-# 
+ testing_Linear_Regression <- function(){
+
+  ## This function is not graded; you can use it to
+  ## test out the 'myLinearRegression' function
+
+  ## Define parameters
+   n    <- 100
+   p    <- 3
+
+   ## Simulate data from our assumed model.
+   ## We can assume that the true intercept is 0
+   X    <- matrix(rnorm(n * p), nrow = n)
+   beta <- matrix(1:p, nrow = p)
+   Y    <- X %*% beta + rnorm(n)
+
+   ## Save R's linear regression coefficients
+   R_coef  <- coef(lm(Y ~ X))
+
+   ## Save our linear regression coefficients
+   my_coef <- myLinearRegression(X, Y)
+
+   ## Are these two vectors different?
+   sum_square_diff <- sum((R_coef - my_coef)^2)
+   if(sum_square_diff <= 0.001){
+     return('Both results are identical')
+   }else{
+     return('There seems to be a problem...')
+   }
+
+ }
+
